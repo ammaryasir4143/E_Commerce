6dc7e349ddecc51dbc4b725d30184798675abaef
mohammadarslan-1 <m.arslan.9160@gmail.com>
2022-05-24 03:32:48 +0500

    Auth Implementation

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@ use App\Http\livewire\HomeComponent;
 use App\Http\livewire\ShopComponent;
 use App\Http\livewire\CartComponent;
 use App\Http\livewire\CheckoutComponent;
+use App\Http\livewire\User\UserDashboardComponent;
+use App\Http\livewire\Admin\AdminDashboardComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +26,24 @@ Route::get('/',HomeComponent::class);
 Route::get('/shop',ShopComponent::class);
 Route::get('/cart',CartComponent::class);
 Route::get('/checkout',CheckoutComponent::class);
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
+
+
+// For User or Customer
+Route::middleware(['auth:sanctum', 'verified'])->group(function(){
+    Route::get('/user/dashboard',UserDashboardComponent::class)->name('user.dashboard');
+});
+
+// For Admin
+Route::middleware(['auth:sanctum', 'verified', 'authadmin'])->group(function(){
+  Route::get('/admin/dashboard',AdminDashboardComponent::class)->name('admin.dashboard');
+});
