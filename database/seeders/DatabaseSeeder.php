5c6e2084f18b9d67ad737f6b90a8e59b228b7894
UsamaNaeem-1 <62239673+UsamaNaeem-1@users.noreply.github.com>
2022-05-29 21:36:10 +0500

    Shop Page
    Making Shop Page Products Dynamic

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+// use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +15,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\Category::factory(6)->create();
+        \App\Models\Product::factory(22)->create();
     }
 }
