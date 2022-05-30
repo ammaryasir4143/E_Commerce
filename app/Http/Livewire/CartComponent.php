c9ac2dec9a7897173135fda445c6fa8e54f7fe35
ammaryasir4143 <105901055+ammaryasir4143@users.noreply.github.com>
2022-05-30 22:19:16 +0500

    Delete Cart Item

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -19,6 +19,15 @@ class CartComponent extends Component
         $qty = $product->qty - 1;
         Cart::update($rowId,$qty);
     }
+    public function destroy($rowId)
+    {
+        Cart::remove($rowId);
+        session()->flash('success_message','Item has been deleted');
+    }
+    public function destroyAll()
+    {
+        Cart::destroy();
+    }
     public function render()
     {
         return view('livewire.cart-component')->layout('layouts.base');
