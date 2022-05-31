48d703820a15c360741d10518a36e7acaae8edb6
Mohammad Arslan <m.arslan.9160@gmail.com>
2022-06-01 00:18:27 +0500

    Error Resolve Dashboard

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ use Illuminate\Support\Facades\Route;
 //Route::get('/', function () {
   //  return view('welcome');
 //});
+
 Route::get('/',HomeComponent::class);
 
 Route::get('/shop',ShopComponent::class);
