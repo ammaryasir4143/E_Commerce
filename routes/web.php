9d95a8df9beea77a0077a3df2f5c63f821c7cc20
ammaryasir4143 <105901055+ammaryasir4143@users.noreply.github.com>
2022-06-07 22:39:51 +0500

    Show All Products in wish list

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,7 @@ use App\Http\Livewire\Admin\AdminProductComponent;
 use App\Http\livewire\DetailsComponent;
 use App\Http\livewire\CategoryComponent;
 use App\Http\livewire\SearchComponent;
+use App\Http\Livewire\WishlistComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -49,6 +50,8 @@ Route::get('/product/{slug}',DetailsComponent::class)->name('product.details');
 Route::get('/product-category/{category_slug}',CategoryComponent::class)->name('product.category');
 
 Route::get('/search',SearchComponent::class)->name('product.search');
+
+Route::get('/wishlist',WishlistComponent::class)->name('product.wishlist');
 
 // Route::middleware([
 //     'auth:sanctum',
